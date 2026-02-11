c8c85e8ecd41cca73f0ffe1101c740d1b43dfd83
yiannis cunning <yiannis2634@gmail.com>
2026-02-11 11:41:54 +0000

    divisible by many 1's problem

--- problem129/main2.c
+++ problem129/main2.c
@@ -24,17 +24,17 @@ typedef struct cord {
 
 // have_need[need_number][lsb]
 // 1, 3, 7, 9
-static uint32_t have_need[10][4] = {
-    {0,0,0,0},
-    {1,7,3,9},
-    {2,4,6,8},
-    {3,1,9,7},
-    {4,8,2,6},
-    {5,5,5,5},
-    {6,2,8,4},
-    {7,9,1,3},
-    {8,6,4,2},
-    {9,3,7,1}
+static uint32_t have_need[10][10] = {
+{0,0,0,0,0,0,0,0,0,0},
+{0,1,0,7,0,0,0,3,0,9},
+{0,2,0,4,0,0,0,6,0,8},
+{0,3,0,1,0,0,0,9,0,7},
+{0,4,0,8,0,0,0,2,0,6},
+{0,5,0,5,0,0,0,5,0,5},
+{0,6,0,2,0,0,0,8,0,4},
+{0,7,0,9,0,0,0,1,0,3},
+{0,8,0,6,0,0,0,4,0,2},
+{0,9,0,3,0,0,0,7,0,1}
 };
 
 void passert(bool cond, char *msg){
@@ -53,14 +53,28 @@ void passert(bool cond, char *msg){
 
 uint32_t a_of(uint32_t n){
     passert(n % 2 != 0, "N given is divisible by 2");
-    passert(n % 5 != 0, "N given is divisible by 2");
+    passert(n % 5 != 0, "N given is divisible by 5");
     // How many times to multiply to get a 111's number
     // = 10**9 + 10**8 + 10**7
     // R(k) = 1111
     // R(k + 1) = R(k)*10 + 1
 
 
-    while(running_sum != 1s)
+    uint32_t have = n % 10;
+    uint32_t running_sum = 0;
+    uint32_t num_ones = 0;
+    uint32_t need = 0;
+
+    while(running_sum != 1){
+        need = (11 - (running_sum % 10))%10;
+        running_sum += have_need[need][have]*n;
+        running_sum = (running_sum - 1)/10;
+        num_ones += 1;
+        //printf("loop  - sum %d\n", running_sum);
+        
+    }
+
+    return num_ones + 1;
 }
 
 
@@ -69,7 +83,24 @@ int main(){
 
 
 
-    printf("Probability of a chip having 3+ defects for %d chips, %d defects = %.12f\n" );
+    int n = 999001;
+    int xmax = 0;
+    while(1){
+        int x = a_of(n);
+        if(x > xmax){
+            printf("Found new x max. n =  %d, A(n) = %d\n", n, x);
+            xmax = x;
+        }
+        n = n + 2;
+        if(n % 5 == 0){
+            n = n + 2;
+        }
+    }
+    
+    printf("Calculating A(n) for n = 3, %d\n", a_of(3) );
+    printf("Calculating A(n) for n = 5, %d\n", a_of(7) );
 
 
 }
+
+// 473411 - 473410
